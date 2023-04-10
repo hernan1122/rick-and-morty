02d477e5b92d073300ebe41b0ab20a052451540d
Axel Torrico <torricohernan2@gmail.com>
2023-04-10 15:36:28 -0300

    agregue Search.css

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import '../styles/Search.css'
 
 export function Search(props) {
   return (
     <div className="Search">
       <input
         type="text"
+        placeholder='Busca un personaje!!'
         ref={props.searchInput}
         value={props.search}
         onChange={props.handleSearch}
